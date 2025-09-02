99d7e8f93574856141ff37a4966cd6aa8fc3c5ef
aymane elmoustaqil <120176362+medm212@users.noreply.github.com>
2025-09-02 14:56:00 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+
+namespace Tp_2
+{
+    public interface IRentable
+    {
+        public double CalculateRent(int duree);
+        
+    }
+    public class Livre :IRentable
+    {
+        public double prix;
+        public string nom;
+        public Livre(string nom , double prix)
+        {
+            this.nom = nom; 
+            this.prix = prix;  // prix par jour 
+        }
+        public double CalculateRent(int duree)// durée par jour 
+        {
+            return prix * duree;
+        }
+    }
+    public class Disque : IRentable
+    {
+        public double prix;
+        public string nom;
+        public Disque(string nom, double prix)
+        {
+            this.nom = nom;
+            this.prix = prix;  // prix par jour 
+        }
+        public double CalculateRent(int duree)// durée par jour 
+        {
+            return prix * duree;
+        }
+    }
+    public class Video: IRentable
+    {
+        public double prix;
+        public string nom;
+        public Video(string nom, double prix)
+        {
+            this.nom = nom;
+            this.prix = prix;  // prix par jour 
+        }
+        public double CalculateRent(int duree)// durée par jour 
+        {
+            return prix * duree;
+        }
+    }
+    public abstract class Publication
+    {
+        public string Name { get; set;}
+        public double Price { get; set; }
+        public Publication (string name, double price)
+        {
+            Name = name;
+            Price = price;
+        
+
+        }
+        public abstract void Publierdetail();
+
+    }
+    public class BookPub : Publication
+    {
+        public BookPub(string name, double price) : base(name, price )
+        {
+        }
+        public override void Publierdetail()
+        {
+            Console.WriteLine($" Livre : {Name}, Prix : {Price} €");
+        }
+    }
+    public class Videopub : Publication
+    {
+        public Videopub(string name, double price ) : base(name, price) { }
+        public override void Publierdetail()
+        {
+            Console.WriteLine($" Video : {Name}, Prix : {Price} €");
+        }
+    }
+    public class Disopub : Publication
+    {
+        public Disopub(string name, double price) : base(name, price) { }
+        public override void Publierdetail()
+        {
+            Console.WriteLine($" Disque : {Name}, Prix : {Price} €");
+        }
+    }
+    public delegate double DiscountStrategy(Publication article);
+    public static class DiscountMethods
+    {
+        
+        public static double RemiseFixe(Publication article)
+        {
+            return article.Price * 0.9;
+        }
+
+        
+        public static double RemiseLivre(Publication article)
+        {
+            if (article is BookPub)
+                return article.Price * 0.8;
+            return article.Price; 
+        }
+
+       
+        public static double PasDeRemise(Publication article)
+        {
+            return article.Price;
+        }
+    }
+
+
+}
+
--- Program.cs
+++ Program.cs
@@ -1,200 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using Tp_2; 
 
-using System;
-
-namespace tp1_exo1
+class Program
 {
-    class Article
+    static void Main()
     {
-        protected string designation;
-        protected int prix;
+        // --- Exercice 1 ---
+        Livre livre1 = new Livre("Les Misérables", 2.5);
+        Video video1 = new Video("Maitre Gims", 0.025);
+        Disque disque1 = new Disque("Cheb Mami", 1.5);
+        int duree = 5;
 
-        public Article(string des, int prix) // constructeur public correct
+        double cout1 = livre1.CalculateRent(duree);
+        double cout2 = video1.CalculateRent(duree);
+        double cout3 = disque1.CalculateRent(duree);
+
+        Console.WriteLine($"Vous allez payer : {cout1} € pour {duree} jours de location du livre {livre1.nom}");
+        Console.WriteLine($"Vous allez payer : {cout2} € pour {duree} jours de location de la vidéo {video1.nom}");
+        Console.WriteLine($"Vous allez payer : {cout3} € pour {duree} jours de location du disque {disque1.nom}");
+
+        Console.WriteLine("\n--- Exercice Classes Abstraites ---");
+
+        // --- Exercice 2  ---
+        List<Publication> publications = new List<Publication>()
         {
-            designation = des;
-            this.prix = prix;
-        }
+            new BookPub("Harry Potter", 15.5),
+            new Videopub("Inception", 12.0),
+            new Disopub("Thriller - MJ", 10.0)
+        };
 
-        public void Acheter()
+        foreach (var pub in publications)
         {
-            Console.WriteLine($"Vous achetez : {designation} pour {prix}€");
-        }
-    }
-
-    class Livre : Article
-    {
-        protected int isbn;
-        protected int nbpage;
-
-        public Livre(int isbn, int nbpage, string des, int prix) : base(des, prix)
-        {
-            this.isbn = isbn;
-            this.nbpage = nbpage;
-        }
-    }
-
-    class Poche : Livre
-    {
-        private string categorie;
-
-        public Poche(string categorie, int isbn, int nbpage, string des, int prix)
-            : base(isbn, nbpage, des, prix)
-        {
-            this.categorie = categorie;
-        }
-    }
-
-    class Broche : Livre
-    {
-        public Broche(int isbn, int nbpage, string des, int prix)
-            : base(isbn, nbpage, des, prix)
-        {
-        }
-    }
-
-    class Disque : Article
-    {
-        protected string label;
-
-        public Disque(string label, string des, int prix) : base(des, prix)
-        {
-            this.label = label;
-        }
-
-        public void Ecouter()
-        {
-            Console.WriteLine($"Écoute du disque : {designation} ({label})");
-        }
-    }
-
-    class Video : Article
-    {
-        protected int duree;
-
-        public Video(int duree, string des, int prix) : base(des, prix)
-        {
-            this.duree = duree;
-        }
-
-        public void Afficher()
-        {
-            Console.WriteLine($"Vidéo : {designation}, Durée : {duree} min, Prix : {prix}€");
-        }
-    }
-    class Program
-    {
-        static void Main()
-        {
-            Article a1 = new Article("Stylo", 2);
-            a1.Acheter();
-            Livre l1 = new Livre(123456, 300, "Le C#", 25);
-            l1.Acheter();
-            Poche p1 = new Poche("Roman", 654321, 200, "Harry Potter", 10);
-            p1.Acheter();
-
-            Broche b1 = new Broche(789012, 400, "Programmation .NET", 30);
-            b1.Acheter();
-
-            Disque d1 = new Disque("Sony Music", "Best of 2025", 15);
-            d1.Acheter();
-            d1.Ecouter();
-
-            Video v1 = new Video(120, "Film d'action", 20);
-            v1.Acheter();
-            v1.Afficher();
-
-        }
-    }
-}
-
-
-
-namespace tp1_exo2
-{
-    struct Article
-    {
-        public string nom;
-        public double prix;
-        public int quantite;
-
-        // Constructeur
-        public Article(string nom, double prix, int quantite)
-        {
-            this.nom = nom;
-            this.prix = prix;
-            this.quantite = quantite;
-        }
-
-        // Méthode Afficher
-        public void Affich()
-        {
-            Console.WriteLine($"Article : {nom}, Prix : {prix}€, Quantité : {quantite}");
-        }
-
-        // Méthode Retirer
-        public void Retirer(int q)
-        {
-            if (q > 0 && q <= quantite)
-                quantite -= q;
-            else
-                Console.WriteLine("Quantité à retirer invalide.");
-        }
-
-        // Méthode Ajouter
-        public void Ajouter(int q)
-        {
-            if (q > 0)
-                quantite += q;
-            else
-                Console.WriteLine("Quantité à ajouter invalide.");
-        }
-    }
-
-
-}
-
-
-
-namespace tp1_exo3
-{
-    enum TypeArticle
-    {
-        Alimentaire,
-        Droguerie,
-        Habillement,
-        Loisir
-    }
-
-    struct ArticleType
-    {
-        public string Nom;
-        public double Prix;
-        public int Quantite;
-        public TypeArticle Type; 
-        public ArticleType(string nom, double prix, int quantite, TypeArticle type)
-        {
-            Nom = nom;
-            Prix = prix;
-            Quantite = quantite;
-            Type = type;
-        }
-
-        
-        public void Afficher()
-        {
-            Console.WriteLine($"Article : {Nom}, Prix : {Prix}€, Quantité : {Quantite}, Type : {Type}");
-        }
-
-        public void Ajouter(int q)
-        {
-            if (q > 0)
-                Quantite += q;
-        }
-
-        public void Retirer(int q)
-        {
-            if (q > 0 && q <= Quantite)
-                Quantite -= q;
+            pub.Publierdetail();
         }
     }
 }
